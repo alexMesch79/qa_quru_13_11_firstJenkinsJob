eede5c86925aa037a125db71b2be705b8bf6703c
AlexeyM <alex_mesch@mail.ru>
2022-08-14 00:08:12 +0300

    added Selenoid

--- src/test/java/tests/TestsBase.java
+++ src/test/java/tests/TestsBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.FormPageTest;
 
 public class TestsBase {
@@ -11,7 +12,12 @@ public class TestsBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1200";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
 }
